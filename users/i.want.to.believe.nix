4e56cc2a6fb5c4a511ff11505d4833bcc5e2eabe
I-Want-ToBelieve <floatshuyin@gmail.com>
2023-11-03 22:09:51 +0800

    update

--- users/i.want.to.believe.nix
+++ users/i.want.to.believe.nix
@@ -8,37 +8,36 @@
     description = "I Want To Believe";
     shell = pkgs.fish;
 
+    isNormalUser = true;
+
+    initialPassword = "nixos";
+
+    createHome = true;
+
+    # compatibility with old drives
+    uid = 1000;
+
+    extraGroups = [
+      "wheel"
+      "nix"
+      "systemd-journal"
+      "adbusers" # android bits
+      "audio"
+      "video"
+      "dialout" # ttyACM*
+      "docker"
+      "libvirtd"
+      "lp" # SANE
+      "networkmanager"
+      "piavpn"
+      "render"
+      "render"
+      "scanner"
+      "vboxusers"
+      "wireshark"
+      "mysql"
+      "lxd" # waydroid
+    ];
     # passwordFile = config.age.secrets.i.want.to.believe.path;
   };
-
-  isNormalUser = true;
-
-  initialPassword = "nixos";
-
-  createHome = true;
-
-  # compatibility with old drives
-  uid = 1000;
-
-  extraGroups = [
-    "wheel"
-    "nix"
-    "systemd-journal"
-    "adbusers" # android bits
-    "audio"
-    "video"
-    "dialout" # ttyACM*
-    "docker"
-    "libvirtd"
-    "lp" # SANE
-    "networkmanager"
-    "piavpn"
-    "render"
-    "render"
-    "scanner"
-    "vboxusers"
-    "wireshark"
-    "mysql"
-    "lxd" # waydroid
-  ];
 }
