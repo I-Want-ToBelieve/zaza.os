e314c5521d709d2612f898e3cd00f0007a5a168b
I-Want-ToBelieve <floatshuyin@gmail.com>
2023-11-04 21:44:54 +0800

    fix: stylix

--- profiles/nixos/stylix.nix
+++ profiles/nixos/stylix.nix
@@ -32,14 +32,4 @@
       name = "Noto Color Emoji";
     };
   };
-
-  # https://danth.github.io/stylix/configuration.html
-  # https://danth.github.io/stylix/options/hm.html
-  stylix.targets.vscode.enable = false;
-  stylix.targets.waybar.enable = false;
-  stylix.targets.swaylock.enable = false;
-  stylix.targets.vim.enable = false;
-  stylix.targets.gtk.enable = false;
-  stylix.targets.kde.enable = false;
-  stylix.targets.xfce.enable = false;
 }
