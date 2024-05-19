6d504f1c2204b630a151ec587339d8004b06bfde
I-Want-ToBelieve <floatshuyin@gmail.com>
2024-05-19 15:53:19 +0800

    feat: autologin

--- profiles/nixos/display-managers/sddm.nix
+++ profiles/nixos/display-managers/sddm.nix
@@ -1,4 +1,19 @@
 {...}: {
   services.xserver.enable = true;
-  services.displayManager.sddm = {enable = true;};
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      settings = {
+        Autologin = {
+          Session = "plasmawayland";
+          User = "i.want.to.believe";
+        };
+      };
+    };
+    defaultSession = "plasmawayland";
+    autoLogin = {
+      enable = true;
+      user = "i.want.to.believe";
+    };
+  };
 }
