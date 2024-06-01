d26d6a2f8fc57e3be59710c1fbcb467a2cf1f7ab
I-Want-ToBelieve <floatshuyin@gmail.com>
2024-06-01 18:44:02 +0800

    fix: autologin

--- profiles/nixos/display-managers/sddm.nix
+++ profiles/nixos/display-managers/sddm.nix
@@ -1,8 +1,15 @@
 {...}: {
   services.xserver.enable = true;
   services.displayManager = {
+    defaultSession = "plasmawayland";
+    autoLogin = {
+      enable = true;
+      user = "i.want.to.believe";
+    };
     sddm = {
       enable = true;
+      wayland.enable = true;
+
       # settings = {
       #   Autologin = {
       #     Session = "plasmawayland";
