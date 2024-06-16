51af8a86e4889dad0e6ba36eea8510a4c0a75123
I-Want-ToBelieve <floatshuyin@gmail.com>
2024-06-16 14:09:58 +0800

    ip

--- hosts/nixos/dell-makcoo/disk-config.nix
+++ hosts/nixos/dell-makcoo/disk-config.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  disks ? ["/dev/sda"], # --arg disks '[ "/dev/sda" ]'
+  disks ? ["/dev/nvme0n1"], # --arg disks '[ "/dev/sda" ]'
   # https://github.com/nix-community/nixos-anywhere-examples/blob/37894d6ddea2beee33c7e2a9faf42d35c40c074f/flake.nix#L20
   #    { disko.devices.disk.main.device = "/dev/vda"; }
   ...
