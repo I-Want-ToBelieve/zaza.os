42970bb5cd9b53c47319edd92a1507b4283c75af
I-Want-ToBelieve <floatshuyin@gmail.com>
2024-06-19 15:47:42 +0800

    unlock

--- scripts/makcoo/unlock.ts
+++ scripts/makcoo/unlock.ts
@@ -1,3 +1,11 @@
+import {
+  $,
+  async,
+  cd,
+  fs,
+  io,
+  path
+} from 'https://deno.land/x/dzx@0.4.0/mod.ts'
 
 import { schools } from './computer.ts'
 import { unlock } from './fn.ts'
@@ -5,11 +13,11 @@ import { unlock } from './fn.ts'
 if (Deno.args.length === 0) Deno.exit(1)
 
 const room = Deno.args.at(0)
-
-const the_room = schools.find(it => String(it.room) === room)
-
+console.log('room', room)
+const the_room = schools.find(it => String(it.room) === String(room))
+console.log('the_room', the_room)
 if (!the_room) Deno.exit(1)
 
-await Promise.allSettled(the_room.hosts.map(it => unlock(it.up)))
+await Promise.allSettled(the_room.hosts.map(it => unlock(it.ip)))
 
 Deno.exit(0)
