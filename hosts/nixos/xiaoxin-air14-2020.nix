88ca9ce06e73c6f2f73cd7e80f374124a47c31b0
I-Want-ToBelieve <floatshuyin@gmail.com>
2024-08-25 20:30:30 +0800

    xiaoxin air14 2020 remove opengl

--- hosts/nixos/xiaoxin-air14-2020.nix
+++ hosts/nixos/xiaoxin-air14-2020.nix
@@ -56,16 +56,6 @@ in {
   };
 
   hardware = {
-    opengl = {
-      enable = true;
-      extraPackages = with pkgs; [
-        intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-    };
-
     steam-hardware = {enable = true;};
 
     bluetooth = {
