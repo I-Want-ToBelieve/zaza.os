c0d2e8b950008f5faad13acec871bb8cfc65c6d5
I-Want-ToBelieve <floatshuyin@gmail.com>
2024-08-26 20:09:20 +0800

    nix on droid

--- hosts/termux/redmi-k70pro/home.nix
+++ hosts/termux/redmi-k70pro/home.nix
@@ -5,7 +5,29 @@
   ...
 }: {
   # Read the changelog before changing this value
-  home.stateVersion = "24.05";
+  home = {
+    stateVersion = "24.05";
+
+    programs = {
+      ssh.enable = true;
+
+      gpg = {
+        enable = true;
+        homedir = "${config.xdg.dataHome}/gnupg";
+      };
+    };
+
+    xdg = {
+      enable = true;
+      userDirs = {
+        enable = true;
+        createDirectories = true;
+        extraConfig = {
+          XDG_DEVELOPMENT_DIR = "${config.xdg.userDirs.documents}/Dev";
+        };
+      };
+    };
+  };
 
   # insert home-manager config
   programs.fish.enable = true;
